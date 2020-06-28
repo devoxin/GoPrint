12a8cac64e9fff6ba25d9dbc3ce06c15fa9ab705
Devoxin <lukel9914@gmail.com>
2020-06-28 22:17:54 +0100

    600ms gen times -> 70ms

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module goprint
 
+go 1.14
+
 require (
 	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
--- printer.go
+++ printer.go
@@ -1,107 +1,103 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "github.com/qeesung/image2ascii/convert"
-    "image"
-    "net/http"
-    "net/url"
-    _ "image/jpeg"
-    _ "image/png"
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"math"
+	"net/http"
+	"net/url"
+
+	"github.com/qeesung/image2ascii/convert"
 )
 
+var httpClient = http.Client{}
+
 func isURL(str string) bool {
-    u, err := url.Parse(str)
-    return err == nil && u.Scheme != "" && u.Host != ""
+	u, err := url.Parse(str)
+	return err == nil && u.Scheme != "" && u.Host != ""
 }
 
-func render(options *convert.Options, image image.Image) (string, error) {
-    converter := convert.NewImageConverter()
-    result := converter.Image2ASCIIString(image, options)
+func render(options *convert.Options, image image.Image) string {
+	width, height := 300.0, 200.0
+	target := 2000.0
+	divider := math.Sqrt(width*height) / math.Sqrt(target)
+	newWidth, newHeight := int(math.Floor(width/divider)), int(math.Floor(height/divider))
 
-    if len(result) > 2000 {
-        if options.Ratio <= 0.0 {
-            return "", errors.New("Couldn't scale image below 2000 characters")
-        }
+	options.FixedHeight = newHeight
+	options.FixedWidth = newWidth
 
-        options.Ratio -= 0.05
-        return render(options, image)
-    }
-    
-    return result, nil
+	converter := convert.NewImageConverter()
+	result := converter.Image2ASCIIString(image, options)
+
+	return result
 }
 
 func downloadImage(url string) (image.Image, error) {
-    res, err := http.Get(url)
+	res, err := httpClient.Get(url)
 
-    if err != nil {
-        return nil, err
-    }
-    
-    if res.StatusCode != 200 {
-        return nil, fmt.Errorf("Server returned %d when fetching image", res.StatusCode)
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Server returned %d when fetching image", res.StatusCode)
+	}
 
-    imageData, imageType, err := image.Decode(res.Body)
+	defer res.Body.Close()
 
-    if err != nil {
-        return nil, err
-    }
+	imageData, imageType, err := image.Decode(res.Body)
 
-    if imageType != "png" && imageType != "jpg" {
-        return nil, fmt.Errorf("Unsupported image type %s", imageType)
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    return imageData, nil
+	if imageType != "png" && imageType != "jpg" && imageType != "jpeg" {
+		return nil, fmt.Errorf("Unsupported image type %s", imageType)
+	}
+
+	return imageData, nil
 }
 
 func handlePrinter(w http.ResponseWriter, req *http.Request) {
-    keys, ok := req.URL.Query()["url"]
+	keys, ok := req.URL.Query()["url"]
 
-    if len(keys) == 0 {
-        w.WriteHeader(400)
-        w.Write([]byte("You must provide a URL to an image"))
-        return
-    }
+	if len(keys) == 0 {
+		w.WriteHeader(400)
+		w.Write([]byte("You must provide a URL to an image"))
+		return
+	}
 
-    imageURL := keys[0]
+	imageURL := keys[0]
 
-    if !ok || len(imageURL) == 0 || !isURL(imageURL) {
-        w.WriteHeader(400)
-        w.Write([]byte("Invalid URL provided"))
-        return
-    }
+	if !ok || len(imageURL) == 0 || !isURL(imageURL) {
+		w.WriteHeader(400)
+		w.Write([]byte("Invalid URL provided"))
+		return
+	}
 
-    image, err := downloadImage(imageURL)
+	image, err := downloadImage(imageURL)
 
-    if err != nil {
-        w.WriteHeader(400)
-        w.Write([]byte(err.Error()))
-        return
-    }
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-    convertOptions := convert.DefaultOptions
-    convertOptions.Colored = false
-    result, err := render(&convertOptions, image)
+	convertOptions := convert.DefaultOptions
+	convertOptions.Colored = false
+	result := render(&convertOptions, image)
 
-    if err != nil {
-        w.WriteHeader(400)
-        w.Write([]byte(err.Error()))
-        return
-    }
-
-    w.WriteHeader(200)
-    w.Write([]byte(result))
+	w.WriteHeader(200)
+	w.Write([]byte(result))
 }
 
 func main() {
-    http.HandleFunc("/printer", handlePrinter)
-    err := http.ListenAndServe(":7080", nil)
+	http.HandleFunc("/printer", handlePrinter)
+	err := http.ListenAndServe(":7080", nil)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
